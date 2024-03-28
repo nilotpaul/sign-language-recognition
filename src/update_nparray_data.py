3bdaa105138b446b5efface0e21f17d74a57b6c0
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-03-28 11:36:35 +0530

    feat: model structure

--- src/update_nparray_data.py
+++ src/update_nparray_data.py
@@ -2,9 +2,12 @@ import numpy as np
 import requests
 import cv2
 import json
+import boto3
 from decouple import config
 
 aws_bucket_name = config('AWS_BUCKET_NAME')
+
+s3 = boto3.client('s3')
 
 def create_labels(json_file_path):
     dataset = []
@@ -50,4 +53,30 @@ def create_labels(json_file_path):
         np.save(f'data/images/{word}.npy', img)
         print(f'saved nparray for {word}')
 
-create_labels('data.json')
+def convert_images_to_nparray(json_file, bucket_name, target_size=(224, 224)):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+        for entry in data['data']:
+            word = entry['word']
+            key = entry['key']
+
+            print('Getting bucket folder...')
+            res = s3.list_objects_v2(Bucket=bucket_name, Prefix=key)
+            
+            for obj in res['Contents']:
+                obj_key = obj['Key']
+
+                print('Fetching image...')
+                obj_response = s3.get_object(Bucket=bucket_name, Key=obj_key)
+                
+                image_data = obj_response['Body'].read()
+
+                img = cv2.imdecode(np.frombuffer(image_data, np.uint8), -1)
+                img = cv2.resize(img, target_size)
+                img = img.astype(np.float32) / 255.0 # normalising in range (0, 1)
+
+                np.save(f'data/images/{word}.npy', img)
+
+
+convert_images_to_nparray('data.json', aws_bucket_name)
